92fe46e528a750b53572325017d1884c8645810f
anne <89winteriscoming@gmail.com>
2021-12-11 17:36:57 +0100

    Make a request

--- main.js
+++ main.js
@@ -15,17 +15,14 @@ const app = new Vue({
     },
     created() {
         // Make a request for a user with a given ID
-axios.get('/user?ID=12345')
-.then(function (response) {
-  // handle success
-  console.log(response);
-})
-.catch(function (error) {
-  // handle error
-  console.log(error);
-})
-.then(function () {
-  // always executed
-});
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(function (response) {
+                // handle success
+                console.log(response.data.response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
     }
-})
+});
