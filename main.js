053c59d4027fe3539744ec9345e04f1b291bdca2
anne <89winteriscoming@gmail.com>
2021-12-11 17:05:25 +0100

    Init File

--- main.js
+++ main.js
@@ -4,3 +4,13 @@
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista
 // Bonus
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const app = new Vue ({
+    el: '#root',
+    data: {
+
+    },
+    methods: {
+
+    },
+})
